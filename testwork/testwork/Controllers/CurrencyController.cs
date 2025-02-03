3ff5fafdf78caa29aa531ac3b74d3420f11c116d
Foly <abugaev2002@mail.ru>
2025-02-03 19:58:51 +0300

    Final
    Added:
    -Controllers
    -Currency classes

---
+++ testwork/testwork/Controllers/CurrencyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testwork.Handlers;
+
+namespace testwork.Controllers
+{
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly CurrencyService _service;
+
+        public CurrencyController(CurrencyService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("currencies")]
+        public async Task<IActionResult> GetCurrencies([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            var result = await _service.GetPagedCurrenciesAsync(page, pageSize);
+            if (result == null)
+                return NotFound();
+            return Ok(new { page, pageSize, result });
+        }
+
+        [HttpGet("currency/{id}")]
+        public async Task<IActionResult> GetCurrencyById(string id)
+        {
+            var currency = await _service.GetCurrencyByIdAsync(id);
+            if (currency == null)
+                return NotFound();
+
+            return Ok(currency);
+        }
+    }
+}
---
+++ testwork/testwork/Handlers/CurrencyService.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+
+
+namespace testwork.Handlers
+{
+    public class CurrencyService
+    {
+        private readonly HttpClient _httpClient;
+        private const string URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+        public CurrencyService(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<Dictionary<string, Currency>> GetCurrenciesAsync()
+        {
+            var response = await _httpClient.GetStringAsync(URL);
+            var data = JsonConvert.DeserializeObject<CurrencyResponse>(response);
+            return data?.Valute ?? new();
+        }
+
+        public async Task<IEnumerable<Currency>> GetPagedCurrenciesAsync(int page, int pageSize)
+        {
+            var currencies = await GetCurrenciesAsync();
+            return currencies.Values.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public async Task<Currency?> GetCurrencyByIdAsync(string id)
+        {
+            var currencies = await GetCurrenciesAsync();
+            foreach (var i in currencies.Keys)
+            {
+                if (currencies[i].Id == id)
+                    return currencies[i];
+                
+            }
+            
+            return null;
+        }
+
+    }
+
+    public class CurrencyResponse
+    {
+        [JsonProperty("Valute")]
+        public Dictionary<string, Currency> Valute { get; set; } = new();
+    }
+
+    public class Currency
+    {
+        [JsonProperty("ID")]
+        public string Id { get; set; }
+
+        [JsonProperty("NumCode")]
+        public string NumCode { get; set; }
+
+        [JsonProperty("CharCode")]
+        public string CharCode { get; set; }
+
+        [JsonProperty("Nominal")]
+        public int Nominal { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Value")]
+        public decimal Value { get; set; }
+
+        [JsonProperty("Previous")]
+        public decimal Previous { get; set; }
+    }
+}
